1025bace1574ecbf8fb73c98841fb94403d9cc09
n3rdium <n3rdium@gmail.com>
2024-04-14 22:50:08 +0530

    Added another function

--- NoRTX/ray_handler.py
+++ NoRTX/ray_handler.py
@@ -1,12 +1,16 @@
 import numpy as np
 
 class RayHandler:
-    def __init__(self, resolution, rpp) -> None:
+    def __init__(self, resolution: list[int], rpp: int) -> None:
         """
         This class handles rays by storing their directions and positions in a numpy array.
+
+        Args:
+            resolution (list[int]): The resolution of the screen in pixels.
+            rpp (int): The number of rays to cast per pixel.
         """
         self.rpp = rpp # "Rays Per Pixel"
         self.resolution = resolution
-        
-        self.rays = np.zeros(resolution + [rpp, 6]) # 3d vector, 3d position, color in single array
-        self.pixels = np.zeros(resolution) # What will be rendered onto the screen
+
+        self.rays = np.zeros(tuple(resolution) + [rpp, 6], dtype=np.float64)  # 3d vector, 3d position, color in single array
+        self.pixels = np.zeros(tuple(resolution) + [3], dtype=np.uint8)  # What will be rendered onto the screen
