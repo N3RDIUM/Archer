6b716a98c112677c998d59025db263f8cab6adbb
n3rdium <n3rdium@gmail.com>
2024-06-23 13:31:49 +0530

    Finally added parallelism

--- archer/src/main.rs
+++ archer/src/main.rs
@@ -1,5 +1,6 @@
 use std::time::Instant;
-use image::{ImageBuffer, RgbImage};
+use rayon::prelude::*;
+use image::{RgbImage, Rgb};
 
 use archer::models::sphere::Sphere;
 use archer::models::base::Model;
@@ -9,16 +10,12 @@ use archer::vectors;
 use vectors::Vec2;
 use vectors::Vec3;
 
-fn do_it(camera: &Camera, x: u32, y: u32) -> Ray {
-    return camera.get_ray(Vec2 { x: x as f32, y: y as f32 })
-}
-
 fn main() {
     const RESOLUTION: [u32; 2] = [1920, 1080];
     let white: Vec3 = Vec3::fill(255.0);
     let blue: Vec3 = Vec3 { x: 0.5 * 255.0, y: 0.7 * 255.0, z: 255.0 };
 
-    let mut image: RgbImage = ImageBuffer::new(RESOLUTION[0], RESOLUTION[1]);
+    let mut image = RgbImage::new(RESOLUTION[0], RESOLUTION[1]);
     let mut cam = Camera::new(RESOLUTION);
     cam.update();
 
@@ -29,23 +26,23 @@ fn main() {
 
     let now = Instant::now();
 
-    for x in 0..RESOLUTION[0] {
-        for y in 0..RESOLUTION[1] {
-            let ray: Ray = do_it(&cam, x, y);
+    let pixels = image.par_enumerate_pixels_mut()
+        .map(|(x, y, pixel)| {
+            let ray: Ray = cam.get_ray(Vec2 { x: x as f32, y: y as f32 });
             
             let norm = ray.direction.normalize();
             let a = 0.5 * (norm.y + 1.0);
             let color: Vec3 = Vec3::fill(1.0 - a) * white + Vec3::fill(a) * blue;
-            let mut final_color: [u8; 3] = [color.x as u8, color.y as u8, color.z as u8];
+            *pixel = Rgb([color.x as u8, color.y as u8, color.z as u8]);
             
             let intersect = sphere.intersect(ray);
             if intersect >= 0.0 {
-                final_color = [255, 0, 0];
+                *pixel = Rgb([255, 0, 0]);
             };
+        });
 
-            *image.get_pixel_mut(x, y) = image::Rgb(final_color);
-        }
-    }
+    let mut sth = vec![];
+    pixels.collect_into_vec(&mut sth);
 
     let elapsed = now.elapsed().as_secs_f64();
     let fps: f64 = 1.0 / elapsed;
