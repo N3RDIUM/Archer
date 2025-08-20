de3aff270ed8cd86b18ff8d04f2136299b173e0a
n3rdium <n3rdium@gmail.com>
2025-08-20 07:38:58 +0530

    update shell.nix

--- shell.nix
+++ shell.nix
@@ -1,2 +1,33 @@
-let pkgs = import <nixpkgs> { };
-in pkgs.mkShell { buildInputs = [ pkgs.SDL2 pkgs.SDL2.dev ]; }
+{
+  pkgs ? import <nixpkgs> {} 
+}:
+
+pkgs.mkShell {
+  buildInputs = [
+    pkgs.vulkan-tools           # For vulkaninfo, etc.
+    pkgs.vulkan-loader
+    pkgs.vulkan-validation-layers
+    pkgs.libxkbcommon
+    pkgs.wayland
+    pkgs.xorg.libX11
+    pkgs.xorg.libXcursor
+    pkgs.xorg.libXrandr
+    pkgs.xorg.libXi
+    pkgs.pkg-config
+  ];
+
+  nativeBuildInputs = [
+    pkgs.pkg-config
+  ];
+
+  # Needed for dynamic libraries in some cases
+  LD_LIBRARY_PATH = with pkgs; lib.makeLibraryPath [
+    vulkan-loader
+    wayland
+    libxkbcommon
+    xorg.libX11
+    xorg.libXcursor
+    xorg.libXrandr
+    xorg.libXi
+  ];
+}
